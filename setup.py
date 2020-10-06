64f4275a53112a3b28303d2bee8d670c0835a29d
Frank Reininghaus <frank78ac@googlemail.com>
2020-10-06 23:03:47 +0200

    Version 0.1 released on PyPI
    https://pypi.org/project/simple-env-config/

--- setup.py
+++ setup.py
@@ -1,13 +1,25 @@
 from setuptools import setup, find_packages
 
+def long_description():
+    with open("README.md", "r") as f:
+        return f.read()
+
 setup(
     name="simple_env_config",
     url="https://github.com/freininghaus/simple_env_config",
     description="Make use of environment variables for configuration easy",
+    long_description=long_description(),
+    long_description_content_type="text/markdown",
     keywords="env environment config decorator",
     version="0.1",
     license="MIT",
     author="Frank Reininghaus",
     author_email="frank78ac@googlemail.com",
     packages=find_packages(exclude=["tests"]),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
 )
